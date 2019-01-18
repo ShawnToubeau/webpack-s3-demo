540bec419215dea9d09f60fd4b15923c9af55db0
Shawn Toubeau <shawntoubeau@mac-24.local>
2019-01-18 11:04:48 -0500

    Commiting changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const S3Plugin = require('webpack-s3-plugin');
-require('dotenv').config();
+const AWS = require('aws-sdk');
+// require('dotenv').config();
 
 module.exports = {
     entry: './src/index.js',
@@ -20,25 +21,9 @@ module.exports = {
         }]
     },
     plugins: [
-        new CleanWebpackPlugin(PATHS.build, {
-            root: path.resolve(__dirname, "..")
-        }),
         new HtmlWebpackPlugin({
-        inject: true,
-        template: `${PATHS.public}/index.html`,
-        filename: htmlFile,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true
-            }
+            inject: true,
+            template: `./src/index.html`,
         }),
         new CompressionPlugin({
             test: /\.(css)$/,
@@ -47,9 +32,8 @@ module.exports = {
         }),
         new S3Plugin({
             s3Options: {
-              accessKeyId: process.env.AWS_ACCESS_KEY, // Your AWS access key
-              secretAccessKey: process.env.AWS_SECRET_KEY // Your AWS secret key
-            //   region: 'eu-east-2' // The region of your S3 bucket
+              credentials: new AWS.SharedIniFileCredentials({profile: 'default'}),
+              region: 'us-east-1' // The region of your S3 bucket
             },
             s3UploadOptions: {
               Bucket: 'shawntoubeau', // Your bucket name
